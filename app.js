baa3f56bec6e74c7c8c3d1052809e49f88001ba3
andlicon <andresjzabalac@hotmail.com>
2023-04-19 07:52:50 -0400

    ADD: evento click ADD: cambiar turno ADD: isCasillaSeleccionable ADD: seleccionarCasilla

--- app.js
+++ app.js
@@ -1,1 +1,52 @@
-console.log('a');
+const isCasillaSeleccionable = (event) => {
+    const casilla = event.target;
+    const clasesArray = casilla.classList;
+
+    for(const clase of clasesArray) {
+        if(clase.includes('Jugador')) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+const seleccionarCasilla = (casilla) => {
+    const juego = JSON.parse(window.sessionStorage.getItem('juego'));
+    const turno = juego.primerJugador;
+
+    if(turno) {
+        casilla.classList.add('primerJugador');
+    }
+    else {
+        casilla.classList.add('segundoJugador');
+    }
+}
+
+const cambiarTurno = () => {
+    const juego = JSON.parse(window.sessionStorage.getItem('juego'));
+    juego.primerJugador = juego.primerJugador ? false : true;
+    sessionStorage.setItem('juego', JSON.stringify(juego));
+}
+
+window.addEventListener('load', () => {
+    const tablero = document.querySelector('.contenido');
+    tablero.addEventListener('click', (event) => {
+        if(!isCasillaSeleccionable(event)) {
+            event.preventDefault();
+            return;
+        }
+
+        const casilla = event.target;
+        seleccionarCasilla(casilla);
+        cambiarTurno();
+    });
+
+    const juego = {
+        'primerJugador': true,
+        'puntuajeJugador1': 0,
+        'puntuajeJugador2': 0
+    }
+
+    window.sessionStorage.setItem('juego', JSON.stringify(juego));
+});
